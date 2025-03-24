b6a0947effc6f5967c65b1c7934967f4101a4ce5
Giovanni Policene <giovannicpoli@gmail.com>
2025-03-24 09:39:38 -0300

    initial commit

---
+++ src/test/java/com/policene/cardapio/CardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.policene.cardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
